3a23bcf17dc0a30936bfefd8865501995c0b3dc0
Amir Adilov <amiradilov202@gmail.com>
2025-10-07 23:51:07 +0500

    fix:fixes based on diagram

--- project/commerces/models.py
+++ project/commerces/models.py
@@ -8,17 +8,16 @@ class Address(models.Model):
     city = models.CharField(max_length=100)
     state = models.CharField(max_length=100)
     zip_code = models.CharField(max_length=20)
-    country = models.CharField(max_length=100)
 
     def __str__(self):
-        return f"{self.street}, {self.city}, {self.state}, {self.zip_code}, {self.country}"
+        return f"{self.street}, {self.city}, {self.state}, {self.zip_code}"
     
 class PromoCode(models.Model):
     code = models.CharField(max_length=50, unique=True)
     discount_percentage = models.DecimalField(max_digits=5, decimal_places=2)
     valid_from = models.DateTimeField()
     valid_to = models.DateTimeField()
-    active = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=True)
 
     def __str__(self):
         return self.code
