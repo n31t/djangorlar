57c242ffacf8e359c56784836b3a6c9957526d0d
Amir Adilov <amiradilov202@gmail.com>
2025-11-03 23:43:39 +0500

    feat: custom user

--- djangolar/users/models.py
+++ djangolar/users/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class CustomUserAbstractUser(AbstractUser):
+    phone_number = models.CharField(
+        blank=True,
+        null=True,
+    )
+    date_of_birth = models.DateField(
+        blank=True,
+        null=True,
+    )
+    bio = models.TextField(
+        blank=True,
+        null=True,
+    )
+
+    class Meta:
+        verbose_name = "User (AbstractUser approach)"
+        verbose_name_plural = "Users (AbstractUser approach)"
+        db_table = 'custom_user_abstract_user'
+
+    def __str__(self):
+        return f"{self.username} ({self.email})"
